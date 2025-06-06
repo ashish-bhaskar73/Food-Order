5239b72bf84127267d6132b7aaff1b49a263c4fd
ashish-bhaskar73 <ashishbahskar0123@gmail.com>
2025-06-06 08:21:41 +0530

    modifies user repository

--- src/main/java/com/foodOrder/FoodOrder/Repository/UserRepository.java
+++ src/main/java/com/foodOrder/FoodOrder/Repository/UserRepository.java
@@ -1,10 +1,19 @@
 package com.foodOrder.FoodOrder.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.foodOrder.FoodOrder.model.User;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 
-public interface  UserRepository extends JpaRepository<User, Integer> {
-    
+    // sql query to check user exist or not by username
+    boolean existsByUsername(String username);
+
+    // sql query to check user exist or not by email id
+    boolean existsByEmail(String email);
 }
