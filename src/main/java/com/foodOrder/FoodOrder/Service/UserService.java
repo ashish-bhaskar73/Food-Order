67ac03080a6ec56013b92f8617f3ef5d00b27185
ashish-bhaskar73 <ashishbahskar0123@gmail.com>
2025-06-06 08:41:34 +0530

    update userService class

--- src/main/java/com/foodOrder/FoodOrder/Service/UserService.java
+++ src/main/java/com/foodOrder/FoodOrder/Service/UserService.java
@@ -2,79 +2,21 @@ package com.foodOrder.FoodOrder.Service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.foodOrder.FoodOrder.Exception.DuplicateResourceException;
-import com.foodOrder.FoodOrder.Exception.ResourceNotFoundException;
-import com.foodOrder.FoodOrder.Repository.UserRepository;
-import com.foodOrder.FoodOrder.model.User;
+import com.foodOrder.FoodOrder.DTO.UserRequestDTO;
+import com.foodOrder.FoodOrder.DTO.UserResponseDTO;
 
 @Service
-public class UserService {
+public interface UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    UserResponseDTO createUser(UserRequestDTO request);
 
-    // register user
-    public User registerUser(User user) {
-        boolean userExist = userRepository.existsById(user.getUserId());
-        if (userExist) {
-            throw new DuplicateResourceException("user is already present");
-        }
-        return userRepository.save(user);
-    }
+    UserResponseDTO getUserById(int id);
 
-    // get all usser
-    public List<User> getAllUsers() {
-        // long userCount = userRepository.count();
-        // if (userCount == 0) {
-        //     throw new ResourceNotFoundException("no user is present");
-        // }
-        return userRepository.findAll();
-    }
+    List<UserResponseDTO> getAllUsers();
 
-    // get user by id
-    public User getUserById(int userId) {
-        return userRepository.findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("user not found by Id:" + userId));
-    }
+    UserResponseDTO updateUserById(int id, UserRequestDTO request);
 
-    // update user
-    public User updateUser(User user, int userId) {
-        User updatedUser = userRepository.findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("user not found by Id:" + userId));
-
-        if (user.getAddress() != null && !user.getAddress().isBlank()) {
-            updatedUser.setAddress(user.getAddress());
-        }
-        if (user.getEmail() != null && !user.getEmail().isBlank()) {
-            updatedUser.setEmail(user.getEmail());
-        }
-        if (user.getPassword() != null && !user.getPassword().isBlank()) {
-            updatedUser.setPassword(user.getPassword());
-        }
-
-        if (user.getPhoneNumber() != null && !user.getPhoneNumber().isBlank()) {
-            updatedUser.setPhoneNumber(user.getPhoneNumber());
-        }
-        if (user.getUsername() != null && !user.getUsername().isBlank()) {
-            updatedUser.setUsername(user.getUsername());
-        }
-        return userRepository.save(updatedUser);
-
-    }
-
-    // delete User
-    public void deleteUser(int userId) {
-        if (!userRepository.existsById(userId)) {
-            throw new ResourceNotFoundException("User not found with id: " + userId);
-        }
-        userRepository.deleteById(userId);
-    }
-
+    void deleteUser(int id);
 }
-
-
-
-// part 2 for updateion use DTO for safer side
