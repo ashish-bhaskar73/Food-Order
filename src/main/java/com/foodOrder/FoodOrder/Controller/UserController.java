5c78f88dd4146f881754408c2a4ef93395a24173
ashish-bhaskar73 <ashishbahskar0123@gmail.com>
2025-06-12 16:01:43 +0530

    user controller

--- src/main/java/com/foodOrder/FoodOrder/Controller/UserController.java
+++ src/main/java/com/foodOrder/FoodOrder/Controller/UserController.java
@@ -3,8 +3,8 @@ package com.foodOrder.FoodOrder.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.foodOrder.FoodOrder.DTO.UserRequestDTO;
+import com.foodOrder.FoodOrder.DTO.UserResponseDTO;
 import com.foodOrder.FoodOrder.Service.UserService;
-import com.foodOrder.FoodOrder.model.User;
 
 import jakarta.validation.Valid;
 
@@ -24,31 +25,38 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    // Create user
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO request) {
+        UserResponseDTO response = userService.createUser(request);
+        return ResponseEntity.status(201).body(response);
+    }
+
+
+
+    // get user by id
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable int id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    // get all user
     @GetMapping("/all")
-    public ResponseEntity<List<User>> getAllUsers() {
-        List<User> users = userService.getAllUsers();
-        return new ResponseEntity<>(users, HttpStatus.OK);
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<User> getUserById(@PathVariable int userId) {
-        return new ResponseEntity<>(userService.getUserById(userId), HttpStatus.OK);
+    // update user
+    @PutMapping("/update/{id}")
+    public ResponseEntity<UserResponseDTO> updateUserById(@PathVariable int id,
+            @Valid @RequestBody UserRequestDTO request) {
+        return ResponseEntity.status(200).body(userService.updateUserById(id, request));
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@RequestBody @Valid User user) {
-        return new ResponseEntity<>(userService.registerUser(user), HttpStatus.CREATED);
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteUserById(@PathVariable int id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.ok("User deleted successfully");
     }
 
-    @PutMapping("/update/{userId}")
-    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable int userId) {
-        return new ResponseEntity<>(userService.updateUser(user, userId), HttpStatus.OK);
-    }
-
-    @PostMapping("/delete/{userId}")
-    public String deleteUser(@PathVariable int userId) {
-        userService.deleteUser(userId);
-        return "user deleted successfully" + userId;
-
-    }
 }
