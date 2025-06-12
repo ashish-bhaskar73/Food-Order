f3a7ebaef11363c5ff6e91993c45a2b829591135
ashish-bhaskar73 <ashishbahskar0123@gmail.com>
2025-06-12 14:36:00 +0530

    Update User service Implementaion

---
+++ src/main/java/com/foodOrder/FoodOrder/Service/UserServiceImpl.java
@@ -0,0 +1,118 @@
+package com.foodOrder.FoodOrder.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.foodOrder.FoodOrder.DTO.UserRequestDTO;
+import com.foodOrder.FoodOrder.DTO.UserResponseDTO;
+import com.foodOrder.FoodOrder.Exception.DuplicateResourceException;
+import com.foodOrder.FoodOrder.Exception.InvalidRequestException;
+import com.foodOrder.FoodOrder.Exception.ResourceNotFoundException;
+import com.foodOrder.FoodOrder.Repository.UserRepository;
+import com.foodOrder.FoodOrder.model.User;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public final class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private UserResponseDTO convertToResponseDTO(User user) {
+        UserResponseDTO response = new UserResponseDTO();
+        response.setUserId(user.getUserId());
+        response.setFirstName(user.getFirstName());
+        response.setLastName(user.getLastName());
+        response.setUsername(user.getUsername());
+        response.setPhoneNumber(user.getPhoneNumber());
+        response.setAddress(user.getAddress());
+        response.setEmail(user.getEmail());
+        response.setRole(user.getRole());
+        return response;
+
+    }
+
+    private User convertToEntity(UserRequestDTO request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setPhoneNumber(request.getPhoneNumber());
+        user.setAddress(request.getAddress());
+        user.setRole(request.getRole());
+        return user;
+    }
+
+    // request for create user4
+    @Override
+    public UserResponseDTO createUser(UserRequestDTO request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new DuplicateResourceException("User with email already exists: " + request.getEmail());
+        }
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new DuplicateResourceException("Username already taken: " + request.getUsername());
+        }
+        User user = convertToEntity(request);
+        User savedUser = userRepository.save(user);
+        return convertToResponseDTO(savedUser);
+    }
+
+    // request for get user by id
+    @Override
+    public UserResponseDTO getUserById(int id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User Not Found: " + id));
+
+        return convertToResponseDTO(user);
+    }
+
+    // request to fecth all users
+    @Override
+    public List<UserResponseDTO> getAllUsers() {
+        return userRepository.findAll().stream().map(this::convertToResponseDTO).collect(Collectors.toList());
+    }
+
+    // request to update user using id
+    @Override
+    public UserResponseDTO updateUserById(int id, UserRequestDTO request) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User Not Found: " + id));
+
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+        user.setPhoneNumber(request.getPhoneNumber());
+        user.setAddress(request.getAddress());
+        // when setting authentication then update the logic
+        if (request.getUsername() != null || request.getRole() != null) {
+            throw new InvalidRequestException("Only admin can update the role and username of a user");
+        } else {
+            if (request.getUsername() != null) {
+                user.setUsername(request.getUsername());
+            }
+
+            if (request.getRole() != null) {
+                user.setRole(request.getRole());
+            }
+        }
+
+        return convertToResponseDTO(userRepository.save(user));
+    }
+
+    // request to delete user by id
+    @Override
+    public void deleteUserById(int id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User Can't be deleted: " + id));
+
+        userRepository.delete(user);
+
+    }
+}
