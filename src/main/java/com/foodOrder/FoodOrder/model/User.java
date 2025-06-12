0db92a40a35a1fae1f999431bfdd5d4e8c93ebfe
ashish-bhaskar73 <ashishbahskar0123@gmail.com>
2025-06-12 15:59:07 +0530

    modify role from string to enum

--- src/main/java/com/foodOrder/FoodOrder/model/User.java
+++ src/main/java/com/foodOrder/FoodOrder/model/User.java
@@ -1,6 +1,8 @@
 package com.foodOrder.FoodOrder.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -17,10 +19,17 @@ public class User {
     private int userId;
     @NotBlank(message = "user name is required")
     private String username;
-    @Email(message = "mail should be correct example: abc@gmail.com")
+    @NotBlank(message = "First Name is required")
+    private String firstName;
+    private String LastName;
+    @Email(message = "Mail should be valid example: abc@gmail.com")
     private String email;
     @NotBlank(message = "Password should be required")
     private String password;
     private String phoneNumber;
     private String address;
+    @NotBlank(message = "role can't be null")
+    @Enumerated(EnumType.STRING)
+    // value of role can be enum
+    private Role role;
 }
