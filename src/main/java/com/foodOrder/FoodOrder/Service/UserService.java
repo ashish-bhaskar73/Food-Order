5e77ccbc421570b7482b0dbab54e8cc53fa38a02
ashish-bhaskar73 <ashishbahskar0123@gmail.com>
2025-06-12 16:00:32 +0530

    Update user service dto logics

--- src/main/java/com/foodOrder/FoodOrder/Service/UserService.java
+++ src/main/java/com/foodOrder/FoodOrder/Service/UserService.java
@@ -2,12 +2,10 @@ package com.foodOrder.FoodOrder.Service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import com.foodOrder.FoodOrder.DTO.UserRequestDTO;
 import com.foodOrder.FoodOrder.DTO.UserResponseDTO;
 
-@Service
+
 public interface UserService {
 
     UserResponseDTO createUser(UserRequestDTO request);
@@ -18,5 +16,5 @@ public interface UserService {
 
     UserResponseDTO updateUserById(int id, UserRequestDTO request);
 
-    void deleteUser(int id);
+    void deleteUserById(int id);
 }
